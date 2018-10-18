6db89d3903367e9c589d9c32a7a5e69101ddb73b
wilkinsb <brandwilki@gmail.com>
2018-10-18 15:24:13 -0700

    finish variable module in tutorial

---
+++ src/js_fundamentals/variables.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Task: Working with Variables
+// 1. Declare two variables: admin and name.
+// 2. Assign the value "John" to name.
+// 3. Copy the value from name to admin.
+// 4. Show the value of admin using alert (must output “John”).
+
+let admin, name;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+// Task: Giving the Right Name
+// 1. Create the variable with the name of our planet. How would you name such a variable?
+// 2. Create the variable to store the name of the current visitor. How would you name that variable?
+
+let ourPlanetName = "Earth";
+
+let currentUserName = "John";
+
+// Task: Uppercase Const?
+// Rules: const should be UPPERCASE when it is hardcoded and does not change
+//   If the variable is determined at run-time, it does not have to be UPPER
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(birthday);
+
